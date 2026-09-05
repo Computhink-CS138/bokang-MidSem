5c082b2a49457e7656ccee62a6ad62477f1ea317
Computhink-CS138 <cs138@computhink.com.sg>
2026-09-05 07:01:18 +0000

    Sep 5, 2026, 7:01 AM

--- testquestion2.js
+++ testquestion2.js
@@ -5,8 +5,6 @@ let Tfactor = 1
 function setup() {
     createCanvas(600, 600);
     background("grey")
-}
-function draw() {
     for (i = 0; i < 10; i++) {
         circle(circleX * Tfactor, circleY * Tfactor, 50);
         Tfactor = Tfactor + 1
