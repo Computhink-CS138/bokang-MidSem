33fa1a758450182e6c813d7a7172ac142ef845d1
Computhink-CS138 <cs138@computhink.com.sg>
2026-09-05 06:42:12 +0000

    Sep 5, 2026, 6:42 AM

--- testquestion1.js
+++ testquestion1.js
@@ -8,7 +8,7 @@ function setup() {
 function draw() {
     circle(150, 200, 100);
 
-    triangle(250, 200, 200, 150, 250, 250); 
+    triangle(250, 200, 200, 150, 200, 250); 
 
 
     // fill("black");
